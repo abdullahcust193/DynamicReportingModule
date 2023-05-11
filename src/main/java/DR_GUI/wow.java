21b137196f8606fcd38b99845b5a8ecd37c84cec
Abdullah Imran <121086610+abdullahimransadiq@users.noreply.github.com>
2023-05-11 06:40:22 +0500

    cdfhdh

---
+++ src/main/java/DR_GUI/wow.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DR_GUI;
+
+/**
+ *
+ * @author chabd
+ */
+public class wow {
+    public int q;
+}
